2ea6f871e7334c41a371a5fa7f70e63b65020abe
tbkizle <tbkizle@gmail.com>
2025-02-25 17:21:29 -0500

    Update Invited to Tracking Code

--- whodat.lua
+++ whodat.lua
@@ -6,32 +6,26 @@ local ff = CreateFrame("Frame")
 
 local function InvitedTo(self, event, searchResultID, newStatus, oldStatus, groupName)
     if not _G.DoCharacters.whodat or _G.DoCharacters.whodat == false then return end
-    if _G.DoCharacters then
-    else
-        _G.DoCharacters = {}
-    end
-    if _G.DoCharacters.whodattracker then
-    else
-        _G.DoCharacters.whodattracker = {}
-    end
     --print(event, searchResultID, newStatus, oldStatus, groupName)
+    --name="|Kq92|k", is unknown
     if newStatus == "invited" and groupName ~= "Unknown" then
         local ResultInfo = searchResultID and C_LFGList.GetSearchResultInfo(searchResultID) or {}
-        local ActivityFullName = ResultInfo["activityID"] and C_LFGList.GetActivityFullName(ResultInfo["activityID"]) or ""
-        local ActivityleaderName = ResultInfo["leaderName"] or ""
-        local ActivitygroupName = groupName and groupName or ""
-        _G.DoCharacters.whodattracker[searchResultID] = { ActivityFullName = ActivityFullName , ActivitygroupName = ActivitygroupName, ActivityleaderName = ActivityleaderName }
-        DEFAULT_CHAT_FRAME:AddMessage("DoKeys: Received Invite To: " ..  _G.DoCharacters.whodattracker[searchResultID].ActivitygroupName .. " " .. _G.DoCharacters.whodattracker[searchResultID].ActivityFullName .. " By: " .. _G.DoCharacters.whodattracker[searchResultID].ActivityleaderName,1,1,0)
+        local map = ResultInfo and ResultInfo.leaderDungeonScoreInfo and ResultInfo.leaderDungeonScoreInfo[1].mapName or ""
+        local ActivityFullName = ResultInfo["activityIDs"] and C_LFGList.GetActivityFullName(ResultInfo["activityIDs"][1]) or ""
+        local ActivityleaderName = ResultInfo and ResultInfo.leaderName or ""
+        local ActivitygroupName = ResultInfo and ResultInfo.name or ""
+        DEFAULT_CHAT_FRAME:AddMessage("DoKeys: Received Invite To: " ..  ActivitygroupName .. " " .. map .. " By: " .. ActivityleaderName,1,1,0)
         --print("DoKeys: Received Invite To: " ..  _G.DoCharacters.whodattracker[searchResultID].ActivitygroupName .. " " .. _G.DoCharacters.whodattracker[searchResultID].ActivityFullName .. " By: " .. _G.DoCharacters.whodattracker[searchResultID].ActivityleaderName)
     end
     if newStatus == "inviteaccepted" then
-        if _G.DoCharacters and _G.DoCharacters.whodattracker and _G.DoCharacters.whodattracker[searchResultID] then
-            local ActivityFullName = _G.DoCharacters.whodattracker[searchResultID].ActivityFullName
-            local ActivityleaderName = _G.DoCharacters.whodattracker[searchResultID].ActivityleaderName
-            local ActivitygroupName = _G.DoCharacters.whodattracker[searchResultID].ActivitygroupName
-            DEFAULT_CHAT_FRAME:AddMessage("DoKeys: Accepted Invite To: " ..  ActivitygroupName .. " " .. ActivityFullName .. " By: " .. ActivityleaderName,1,1,0)
-            --print("DoKeys: Accepted Invite To: " ..  ActivitygroupName .. " " .. ActivityFullName .. " By: " .. ActivityleaderName)
-        end
+        --DevTools_Dump(C_LFGList.GetSearchResultInfo(searchResultID))
+        local ResultInfo = searchResultID and C_LFGList.GetSearchResultInfo(searchResultID) or {}
+        local map = ResultInfo and ResultInfo.leaderDungeonScoreInfo and ResultInfo.leaderDungeonScoreInfo[1].mapName or ""
+        local ActivityFullName = ResultInfo["activityIDs"] and C_LFGList.GetActivityFullName(ResultInfo["activityIDs"][1]) or ""
+        local ActivityleaderName = ResultInfo and ResultInfo.leaderName or ""
+        local ActivitygroupName = ResultInfo and ResultInfo.name or ""
+        DEFAULT_CHAT_FRAME:AddMessage("DoKeys: Accepted Invite To: " ..  ActivitygroupName .. " " .. map .. " By: " .. ActivityleaderName,1,1,0)
+        --print("DoKeys: Accepted Invite To: " ..  ActivitygroupName .. " " .. ActivityFullName .. " By: " .. ActivityleaderName)
     end
 end
 
