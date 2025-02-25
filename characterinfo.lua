27d0e472c5ab28257f6c7647d5b768b7f783336b
tbkizle <tbkizle@gmail.com>
2025-02-25 17:20:20 -0500

    Update RequestPartyKeys

--- characterinfo.lua
+++ characterinfo.lua
@@ -1327,7 +1327,8 @@ local function RequestPartyKeys(_, event)
     and (GetLFGMode(LE_LFG_CATEGORY_SCENARIO) == nil)
     and (GetLFGMode(LE_LFG_CATEGORY_LFR) == nil)
     and UnitInParty("player")
-    and not (select(3, GetInstanceInfo()) == 208) then
+    and not (select(3, GetInstanceInfo()) == 205)
+    and not (select(3, GetInstanceInfo()) == 208)then
         ChatThrottlePlusLib:SendAddonMessage( "NORMAL", "DoKeys", "PartyRequest", "PARTY")
         local openRaidLib = LibStub:GetLibrary("LibOpenRaid-1.0")
         for i=1,_G.GetNumGroupMembers() do
